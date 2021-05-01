9029bbdd5e3718514b657de74cb2d15855a50290
Maruf Marzuq <79480788+marufmarzuq@users.noreply.github.com>
2021-05-02 00:08:51 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let toggle = document.getElementById('toggle');
+let sidebar = document.getElementById('sidebar');
+
+
+document.onclick = function (h) {
+     if (h.target !== toggle && h.target !== sidebar) {
+          toggle.classList.remove('active');
+          sidebar.classList.remove('active');
+     }
+}
+
+
+toggle.onclick = function () {
+     toggle.classList.toggle('active');
+     sidebar.classList.toggle('active');
+}
